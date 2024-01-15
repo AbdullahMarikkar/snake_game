1fa36e34a53ba789e708604c34656c5b687598f0
Abdullah Marikkar <81084836+AbdullahMarikkar@users.noreply.github.com>
2024-01-15 14:42:56 +0530

    high score save and retrieve from a file implemented

--- scoreboard.py
+++ scoreboard.py
@@ -3,11 +3,13 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial",24,"normal")
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -16,12 +18,13 @@ class Scoreboard(Turtle):
         
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}",align=ALIGNMENT,font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}",align=ALIGNMENT,font=FONT)
         
     def reset(self):  
-        if (self.score > self.high_score):
-            self.high_score = self.score 
-            
+        if (self.score > self.highscore):
+            self.highscore = self.score
+            with open("high_score.txt",mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
         
